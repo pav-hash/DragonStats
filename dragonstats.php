d8c77bee378dea1664a7692956a756ac46f65513
pav-hash <34192334+pav-hash@users.noreply.github.com>
2018-05-18 12:54:47 -0700

    Add files via upload

--- dragonstats.php
+++ dragonstats.php
@@ -41,6 +41,11 @@ $show_mac_addy = false;
 $multi_subnets = array();
 
 
+# Condense printing to just basic miner information (excludes hashboards, accepted, 1m and avg hashrate )
+# NOTE: If you are viewing from a ipad or iphone, it will automagically enable condense listing.
+$condense_listings = false;
+
+
 
 #### End of custom defines ####
 
@@ -125,34 +130,43 @@ function find_active_stratum( $what, $_string, $instance ) {
         $count = 1;
         $what = $what . ":";
         foreach ( $_string as $_element ) {
-                if ( strpos( $_element, $what ) !== false ) {
-                        if ( $instance == $count ) {
-				if ( substr_count( $_element, ':' ) > 1 ) {
-					list( $noop, $base, $value ) = explode( ": ", $_element );
-				} else {
-                                	list( $base, $value ) = explode( ": ", $_element );
-				}
-                                return str_replace( "}]", "", $value );
-                        } else {
-                                $count = $count + 1;
-                        }
-                }
-        }
-        return "";
+	if ( strpos( $_element, $what ) !== false ) {
+		if ( $instance == $count ) {
+			if ( substr_count( $_element, ':' ) > 1 ) {
+				list( $noop, $base, $value ) = explode( ": ", $_element );
+			} else {
+				list( $base, $value ) = explode( ": ", $_element );
+			}
+			return str_replace( "}]", "", $value );
+		} else {
+			$count = $count + 1;
+		}
+	}
+}
+return "";
 }
 
 ######################### end functions ##################################
 
+$browser = $_SERVER['HTTP_USER_AGENT'];
+if ( stripos( $browser, 'iphone' ) == true ) {
+$condense_listings = true;
+} elseif ( stripos( $browser, 'ipad' ) == true ) {
+$condense_listings = true;
+} else {
+$browser = 'n/a';
+}
+
 
 if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['get_all_dragon_ips']))
 {
-	get_all_dragon_ips();
-	header('Location:'.$_SERVER['PHP_SELF']);
+get_all_dragon_ips();
+header('Location:'.$_SERVER['PHP_SELF']);
 }
 
 if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['remove_ip']))
 {
-	remove_ip( $_POST['remove_ip'] );
+remove_ip( $_POST['remove_ip'] );
 #	header('Location:'.$_SERVER['PHP_SELF']);
 }
 
@@ -160,7 +174,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['remove_ip']))
 
 # check if the ip_table.lst file exists..
 if (!file_exists('/tmp/dragon_ips.lst')) {
-	get_all_dragon_ips();
+get_all_dragon_ips();
 }	
 
 $ip = get_all_miners();
@@ -176,94 +190,111 @@ $ip = get_all_miners();
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="Content-Script-Type" content="text/javascript" />
 <meta http-equiv="cache-control" content="no-cache" />
-<meta http-equiv="refresh" content="60" />
+<meta http-equiv="refresh" content="20" />
 <link rel="stylesheet" type="text/css" media="screen" href="/css/cascade.css" />
 <title>Dragon Miner</title>
 
 <script type="text/javascript"> 
-	function display_c(){
-		var refresh = 1000; // Refresh rate in milli seconds
-		mytime = setTimeout('display_ct()',refresh)
-	}
+function display_c(){
+	var refresh = 1000; // Refresh rate in milli seconds
+	mytime = setTimeout('display_ct()',refresh)
+}
 
-	function display_ct() {
-		var strcount
-		var x = new Date();
-		var n = x.toDateString();
-		var t = x.toLocaleTimeString();
-		document.getElementById('ct').innerHTML = n + "   " + t;
-		tt = display_c();
-	}
+function display_ct() {
+	var strcount
+	var x = new Date();
+	var n = x.toDateString();
+	var t = x.toLocaleTimeString();
+	document.getElementById('ct').innerHTML = n + "   " + t;
+	tt = display_c();
+}
 </script>
-
-
 </head>
-<body onload=display_ct()>
 
 
-  <div id="maincontainer">
-     <div id="tabmenu">
-        <div class="tabmenu1">
-            <ul class="tabmenu l1">
-                <li class="tabmenu-item-network active"><a href=<?php echo $_SERVER['PHP_SELF'] ?>>Dragon Miner Status</a></li>
-            </ul>
-            <br style="clear: both" />
-        </div>
-     </div>
-     <div id="maincontent">
-        <noscript>
-           <div class="errorbox">
-               <strong>Java Script required!</strong><br /> You must enable Java Script or LuCI will not work properly.
-           </div>
-        </noscript>
+<!-- ############################################ -->
 
-        <h2 style="padding-bottom:10px;"><a id="content" name="content">Status <span style='float: right' id='ct'></span></a></h2>
+<body class="lang_en" onload=display_ct()>
+<p class="skiplink">
+	<span id="skiplink1"><a href="#navigation">Skip to navigation</a></span>
+	<span id="skiplink2"><a href="#content">Skip to content</a></span>
+</p>
+<div id="menubar">
+	<h2 class="navigation"><a id="navigation" name="navigation">Navigation</a></h2>
+	<div class="clear"></div>
+</div>
 
-<table id="ant_devs" class="cbi-section-table">
-<tr><th>
-<form action="dragonstats.php" method="post">
-    <input type="submit" name="get_all_dragon_ips" value="Rescan Network" style="font-size:12pt;color:white; background-color:green;border:2px solid #336600;padding:3px; />
-</form>
-</th></tr>
-</table>
 
-        <div class="cbi-map" id="cbi-cgminerstatus">
-            <!-- tblsection -->
-            <!-- tblsection -->
-            <fieldset class="cbi-section" id="cbi-table-table">
-            <legend>My Miners</legend>
-            <div class="cbi-section-descr"></div>
-            <div class="cbi-section-node">
-            <table id="ant_devs" class="cbi-section-table">
-                <tr class="cbi-section-table-titles">
-                <th class="cbi-section-table-cell">Miner IP</th>
-                <th class="cbi-section-table-cell">Miner Name (Type)</th>
-		<th class="cbi-section-table-cell">Running Since</th>
-                <th class="cbi-section-table-cell">Hashboard #</th>
-                <th class="cbi-section-table-cell">Status</th>
-                <th class="cbi-section-table-cell">Accepted</th>
-                <th class="cbi-section-table-cell">Temp</th>
-		<th class="cbi-section-table-cell">Fan Speed</th>
-		<th class="cbi-section-table-cell">Hashrate (1m)</th>
-		<th class="cbi-section-table-cell">Hashrate (avg)</th>
-		<th class="cbi-section-table-cell">HW Errors</th>
-		<th class="cbi-section-table-cell"></th>
-                </tr>
-                <tr class="cbi-section-table-descr">
-                <th class="cbi-section-table-cell"></th>
-                <th class="cbi-section-table-cell"></th>
-		<th class="cbi-section-table-cell"></th>
-                <th class="cbi-section-table-cell"></th>
-                <th class="cbi-section-table-cell"></th>
-                <th class="cbi-section-table-cell"></th>
-		<th class="cbi-section-table-cell"></th>
-		<th class="cbi-section-table-cell"></th>
-                <th class="cbi-section-table-cell"></th>
-		<th class="cbi-section-table-cell"></th>
-                <th class="cbi-section-table-cell"></th>
-		<th class="cbi-section-table-cell"></th>
-                </tr>
-   
+<div id="maincontainer">
+	<div id="tabmenu">
+		<div class="tabmenu1">
+			<ul class="tabmenu l1">
+				<li class="tabmenu-item-status"><a href="">Home</a></li>
+			</ul>
+			<br style="clear: both" />
+		</div>
+	</div>
+	<div id="maincontent">
+		<noscript>
+			<div class="errorbox">
+				<strong>Java Script required!</strong><br /> You must enable Java Script in your browser or LuCI will not work properly.
+			</div>
+		</noscript>
+		<h2 style="padding-bottom:10px;"><a id="content" name="content">Miner Status</a></h2>
+
+		<p><div align='center'>
+			<form action="<?php $_SERVER['PHP_SELF'] ?>" method='post'> <button type='submit' name='get_all_dragon_ips' value='$ipaddy' class='rescanbtn'>Rescan Network</button></form>
+		</div></p>
+
+		<div class="cbi-map" id="cbi-cgminerstatus">
+			<!-- tblsection -->
+			<fieldset class="cbi-section" id="cbi-table-table">
+				<legend>Summary</legend>
+				<div class="cbi-section-descr"></div>
+				<div class="cbi-section-node">
+					<table class="cbi-section-table">
+						<tr class="cbi-section-table-titles">
+						<th class="cbi-section-table-cell">Miner IP</th>
+						<th class="cbi-section-table-cell">Miner Name (Type)</th>
+						<th class="cbi-section-table-cell">Running Since</th>
+						<th class="cbi-section-table-cell">Current Hashrate</th>
+						<?php if ( $condense_listings == false ) { ?>
+							<th class="cbi-section-table-cell">Hashboard #</th>
+							<th class="cbi-section-table-cell">Status</th>
+							<th class="cbi-section-table-cell">Accepted</th>
+						<?php } ?>
+						<th class="cbi-section-table-cell">Temp</th>
+						<th class="cbi-section-table-cell">Fan Speed</th>
+						<?php if ( $condense_listings == false ) { ?>
+							<th class="cbi-section-table-cell">Hashrate (1m)</th>
+							<th class="cbi-section-table-cell">Hashrate (avg)</th>
+						<?php } ?>
+						<th class="cbi-section-table-cell">HW Errors</th>
+						<th class="cbi-section-table-cell"></th>
+						</tr>
+						<tr class="cbi-section-table-descr">
+						<th class="cbi-section-table-cell"></th>
+						<th class="cbi-section-table-cell"></th>
+						<th class="cbi-section-table-cell"></th>
+						<th class="cbi-section-table-cell"></th>
+						<?php if ( $condense_listings == false ) { ?>
+							<th class="cbi-section-table-cell"></th>
+							<th class="cbi-section-table-cell"></th>
+							<th class="cbi-section-table-cell"></th>
+						<?php } ?>
+						<th class="cbi-section-table-cell"></th>
+						<th class="cbi-section-table-cell"></th>
+						<?php if ( $condense_listings == false ) { ?>
+					                <th class="cbi-section-table-cell"></th>
+					                <th class="cbi-section-table-cell"></th>
+						<?php } ?>
+				                <th class="cbi-section-table-cell"></th>
+				                <th class="cbi-section-table-cell"></th>
+				                </tr>
+
+
+
+<!-- ############################################ -->
 
 
 
@@ -282,7 +313,7 @@ foreach ($ip as $ipaddy ) {
 	## chain number
 	echo "<tr class=\"cbi-section-table-row cbi-rowstyle-1\" id=\"cbi-table-1\">";
 	echo "<td class=\"cbi-value-field\">";
-	echo "<div id=\"cbi-table-1-chain\"><b><a href=\"http://$ipaddy/cgi-bin/minerStatus.cgi\" target=\"_blank\">$ipaddy</a></b></div>";
+	echo "<div id=\"cbi-table-1-chain\"><b><a href=\"http://$ipaddy/\" target=\"_blank\">$ipaddy</a></b></div>";
 	echo "<div id=\"cbip-table-1-chain\"></div>";
 	echo "</tr></td>";
 
@@ -293,6 +324,9 @@ foreach ($ip as $ipaddy ) {
 
 	$_pools = exec('python ./get_stats.py ' . $ipaddy . ' pools 2>&1');
 	$pools = str_replace( "'", "", str_replace( "u'", "", explode(", u",$_pools) ) );
+
+	$_summary = exec('python ./get_stats.py ' . $ipaddy . ' summary 2>&1');
+	$summary = str_replace( "'", "", str_replace( "u'", "", explode(", u",$_summary) ) );
 
         $_fullstats = exec('python ./get_stats.py ' . $ipaddy . ' stats 2>&1');     
         $fullstats = str_replace( "'", "", str_replace( "u'", "", explode(", u",$_fullstats) ) );
@@ -332,13 +366,13 @@ foreach ($ip as $ipaddy ) {
 			}
 		}
 
-                echo "<td class=\"cbi-value-field\">";
+                echo "<td class=\"cbi-value-name\">";
                 if ( $cc == 1 ) {
-                        echo "<div id=\"cbi-table-1-elapsed\">" . $miner_miner . $miner_type . "</div>";
+			echo "<div id=\"cbi-value-name\">" . $miner_miner . $miner_type . "</div>";
                 } else {
-                        echo "<div id=\"cbi-table-1-elapsed\"></div>";
+                        echo "<div id=\"cbi-value-name\"></div>";
                 }
-                echo "<div id=\"cbip-table-1-temp\"></div>";
+                echo "<div id=\"cbi-value-name\"></div>";
                 echo "</td>";
 
                 ## elapsed time
@@ -353,24 +387,38 @@ foreach ($ip as $ipaddy ) {
                 echo "<div id=\"cbip-table-1-temp\"></div>";
                 echo "</td>";
 
+		## current hashrate
+                echo "<td class=\"cbi-value-field\">";
+                if ( $cc == 1 ) {
+			echo "<div class='curr_hashrate'><a class='curr_hw_text'>" . round( find_element("MHS av", $summary ) / 1000000, 2 ) . "</a></div>";
+                } else {
+                        echo "<div id=\"cbi-table-1-temp\"></div>";
+                }
+                echo "<div id=\"cbip-table-1-temp\"></div>";
+                echo "</td>";
 
-		## chain #
-		echo "<td class=\"cbi-value-field\">";
-		echo "<div id=\"cbi-table-1-chain\">$cc</div>";
-		echo "<div id=\"cbip-table-1-chain\"></div>";
-		echo "</td>";
 
-		## status 
-		echo "<td class=\"cbi-value-field\">";
-		echo "<div id=\"cbi-table-1-asic\">" . find_element("Status", $stats, $cc) . "</div>";
-		echo "<div id=\"cbip-table-1-asic\"></div>";
-		echo "</td>";
+		if ( $condense_listings == false ) {
 
-		## accepted 
-		echo "<td class=\"cbi-value-field\">";
-		echo "<div id=\"cbi-table-1-frequency\">" . find_element("Accepted", $stats, $cc) . "</div>";
-		echo "<div id=\"cbip-table-1-frequency\"></div>";
-		echo "</td>";
+			## chain #
+			echo "<td class=\"cbi-value-field\">";
+			echo "<div id=\"cbi-table-1-chain\">$cc</div>";
+			echo "<div id=\"cbip-table-1-chain\"></div>";
+			echo "</td>";
+
+			## status 
+			echo "<td class=\"cbi-value-field\">";
+			echo "<div id=\"cbi-table-1-asic\">" . find_element("Status", $stats, $cc) . "</div>";
+			echo "<div id=\"cbip-table-1-asic\"></div>";
+			echo "</td>";
+
+			## accepted 
+			echo "<td class=\"cbi-value-field\">";
+			echo "<div id=\"cbi-table-1-frequency\">" . find_element("Accepted", $stats, $cc) . "</div>";
+			echo "<div id=\"cbip-table-1-frequency\"></div>";
+			echo "</td>";
+
+		}
 
 		## temp
 		echo "<td class=\"cbi-value-field\">";
@@ -388,17 +436,22 @@ foreach ($ip as $ipaddy ) {
                 echo "<div id=\"cbip-table-1-temp\"></div>";
                 echo "</td>";
 
-                ## hashrate 1m
-                echo "<td class=\"cbi-value-field\">";
-                echo "<div id=\"cbi-table-1-temp\">" . find_element("MHS 1m", $stats, $cc) . "</div>";
-                echo "<div id=\"cbip-table-1-temp\"></div>";
-                echo "</td>";
 
-                ## hashrate avg
-                echo "<td class=\"cbi-value-field\">";
-                echo "<div id=\"cbi-table-1-temp\">" . find_element("MHS av", $stats, $cc) . "</div>";
-                echo "<div id=\"cbip-table-1-temp\"></div>";
-                echo "</td>";
+		if ( $condense_listings == false ) {
+
+			## hashrate 1m
+			echo "<td class=\"cbi-value-field\">";
+			echo "<div id=\"cbi-table-1-temp\">" . find_element("MHS 1m", $stats, $cc) . "</div>";
+			echo "<div id=\"cbip-table-1-temp\"></div>";
+			echo "</td>";
+
+			## hashrate avg
+			echo "<td class=\"cbi-value-field\">";
+			echo "<div id=\"cbi-table-1-temp\">" . find_element("MHS av", $stats, $cc) . "</div>";
+			echo "<div id=\"cbip-table-1-temp\"></div>";
+			echo "</td>";
+
+		}
 
                 ## hw errors 
                 echo "<td class=\"cbi-value-field\">";
@@ -409,8 +462,7 @@ foreach ($ip as $ipaddy ) {
                 ## remove miner ip address
                 echo "<td class=\"cbi-value-field\">";
                 if ( $cc == 2 ) {
-#			echo "<a id=\"remove_ip\" href=" . $_SERVER['PHP_SELF'] . "?remove_ip=" . $ipaddy . ">Remove Miner</a>";
-                        echo "<form action=" . $_SERVER['PHP_SELF'] . " method='post'> <button type='submit' name='remove_ip' value='$ipaddy' class='remove_ip'>Remove Miner</button></form>";
+                        echo "<form action="  . $_SERVER['PHP_SELF'] . " method='post'> <button type='submit' name='remove_ip' value='$ipaddy' class='remove_ip'>Remove Miner</button></form>";
                 } else {
                         echo "<div id=\"cbi-table-1-asic\"></div>";
                 }
@@ -428,15 +480,22 @@ foreach ($ip as $ipaddy ) {
 
 ?>
 
-</table>
-<br><br><br><br>
-<table id="ant_devs" class="cbi-section-table">
-<tr><th><center>
-    Written by pav_hash --  donations gladly accepted..    3CsdpkawMuhSqBrdvCcHacSyuzmjzHVTTR (btc)   DsTyn6kv8NjY83LY4RminGTP2TF7DQBpeAw (dcr) 
-</th></tr>
+                                                </table>
+                                        </div>
+                                </fieldset>
+
+                                <br />
+                        </div>
+                        <div class="clear"></div>
+                </div>
+        </div>
+
+<div class="clear"></div>
+<br><br><br><div style="text-align: center; font-size: 90%; width: 100%;">
+Written by pav_hash --  Donations gladly accepted..<br>3CsdpkawMuhSqBrdvCcHacSyuzmjzHVTTR (btc)<br>DsTyn6kv8NjY83LY4RminGTP2TF7DQBpeAw (dcr) 
+</div>
 
 
-</table>
 </body>
 </html>
 
